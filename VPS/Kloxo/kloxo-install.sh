ab0cdd9067302407507fbc2280db9d46fd9d9e5a
LKTips <38848696+LKTips@users.noreply.github.com>
2018-04-30 11:45:56 +0530

    Create kloxo-install.sh

---
+++ VPS/Kloxo/kloxo-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Starting Kloxo-MR install...
+read -p "press [enter] to start..."
+yum update -y
+yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y
+yum install telnet wget -y
+cd /tmp
+rpm -ivh https://github.com/mustafaramadhan/rpms/raw/master/mratwork/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm
+cd /
+yum clean all
+yum update mratwork-* -y
+yum install kloxomr7 -y
+sh /script/upcp
+
+read -p "Install done! Reboot VPS once done!"
