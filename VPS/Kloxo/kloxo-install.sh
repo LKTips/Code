55fea1f0850ce4403593b1661d21aaed1194481e
LKTips <38848696+LKTips@users.noreply.github.com>
2020-08-28 10:53:55 +0530

    Update kloxo-install.sh

--- VPS/Kloxo/kloxo-install.sh
+++ VPS/Kloxo/kloxo-install.sh
@@ -2,7 +2,7 @@
 echo Starting Kloxo-MR install...
 read -p "press [enter] to start..."
 yum update -y
-yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y 
+yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y
 yum install telnet wget -y
 cd /tmp
 rpm -ivh https://github.com/mustafaramadhan/rpms/raw/master/mratwork/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm
